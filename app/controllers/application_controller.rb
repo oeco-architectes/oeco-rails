ec259a1b837d8514c95eae9e44722b4c2392ca14
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-07-17 18:15:05 -0700

    Added HTTP Basic Authentication on staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # Enable HTTP Basic Authentication if AUTH_USERNAME and AUTH_PASSWORD are set.
+  # This allows protecting staging and review platforms.
+  if ENV.has_key?('AUTH_USERNAME') && ENV.has_key?('AUTH_PASSWORD')
+    http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+  end
 end
