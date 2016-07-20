8da70237bf309e6696b936d9d3cdb9d2a29ebb9d
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-07-20 13:47:20 -0700

    Added fields to Project model

--- db/migrate/20160720204005_create_projects.rb
+++ db/migrate/20160720204005_create_projects.rb
@@ -1,8 +1,14 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
-
-      t.timestamps
+      t.string :slug
+      t.string :title
+      t.string :summary
+      t.string :content
+      t.boolean published: false
+      t.timestamps null: false
     end
+
+    add_index :projects, :slug, unique: true
   end
 end
