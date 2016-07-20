2f46456f12ad7fe7a1f2df23fac3168d45eae449
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-07-20 14:55:17 -0700

    Added /projects/:id view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    puts params.inspect
+    @project = Project.find_by_slug(params[:id])
+  end
 end
