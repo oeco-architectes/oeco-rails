46a4dfaf5bd3833aed894c3d5d5981dbb469482a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-07-20 12:07:11 -0700

    Added news in home#index view

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,9 @@
 class News < ApplicationRecord
   validates :url, presence: true, uniqueness: true
   validates :order, presence: true, uniqueness: true
+
+  def img_url
+    s = Settings.data
+    URI("#{s.base_url}/#{s.news_url}/#{slug}.#{s.img_ext}")
+  end
 end
