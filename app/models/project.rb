9c24ba290095bf32d009979c9a1007b077fe79b8
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-07-29 00:10:01 -0700

    Added simple project mozaic

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,13 @@ class Project < ApplicationRecord
   def to_param
     slug
   end
+
+  def img_url(width, height, index = 0)
+    Settings.data.project_img % {
+      index: index,
+      slug: slug,
+      width: width,
+      height: height
+    }
+  end
 end
