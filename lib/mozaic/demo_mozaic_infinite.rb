be77ea5799ab118cc1b3a401324220dff7785339
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-08-16 15:39:27 -0700

    Added Mozaic generator

---
+++ lib/mozaic/demo_mozaic_infinite.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require_relative './mozaic'
+
+i = 1
+loop do
+  (1..10).each do |columns|
+    (1..200).each do |items|
+      next if 2 * items < columns
+
+      puts ''
+      puts "### (#{i}) #{items} in #{columns} columns"
+      mozaic = Mozaic.create(items, columns)
+      puts mozaic
+      puts "=> valid? #{mozaic.valid?}\n\n"
+      i += 1
+    end
+  end
+end
